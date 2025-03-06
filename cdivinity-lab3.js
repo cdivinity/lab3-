24f887ed3e5170d26642161241df4ce05053b726
cdivinity <140557169+cdivinity@users.noreply.github.com>
2025-03-05 19:20:54 -0600

    index

---
+++ cdivinity-lab3.js
@@ -0,0 +1,35 @@
+//Courtland Divinity
+//ITMD 441
+
+/*
+Excercise 1:
+function minMaxAverage(numbers) {
+    let total = numbers.length;
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+    let sum = numbers.reduce((a, b) => a + b, 0);
+    let average = sum / total;
+    console.log(`Total Numbers: ${total}, Min Value: ${min}, Max Value: ${max}, Average: ${average}`);
+  }
+  
+  minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1]);*/
+
+  
+
+  //Excercise 2:
+
+  // program to count the number of vowels in a string
+/*
+function countVowels(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+      count++;
+    }
+  }
+  return count;
+}
+console.log("hello: " + countVowels("hello") + " vowels.");
+console.log("world: " + countVowels("world") + " vowels.");
+console.log("javascript: " + countVowels("javascript") + " vowels.");
+*/
