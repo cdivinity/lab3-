f57f422f0f5680c89ef394c28033becb0b3a9dfb
cdivinity <140557169+cdivinity@users.noreply.github.com>
2025-03-05 23:59:33 -0600

    Update cdivinity-lab3.js

--- cdivinity-lab3.js
+++ cdivinity-lab3.js
@@ -1,8 +1,10 @@
 //Courtland Divinity
 //ITMD 441
 
-/*
-Excercise 1:
+
+
+
+//Excercise 1:
 function minMaxAverage(numbers) {
     let total = numbers.length;
     let min = Math.min(...numbers);
@@ -11,15 +13,16 @@ function minMaxAverage(numbers) {
     let average = sum / total;
     console.log(`Total Numbers: ${total}, Min Value: ${min}, Max Value: ${max}, Average: ${average}`);
   }
-  
-  minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1]);*/
+  minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1]);
+  minMaxAverage([ 1, 5, 3, 5, 10, 12, 8, 6]);
+  minMaxAverage([ 9, 45, 89, 7, 100, 15, 89, 0]);
 
-  
 
-  //Excercise 2:
 
-  // program to count the number of vowels in a string
-/*
+
+
+
+//Excercise 2:
 function countVowels(str) {
   let count = 0;
   for (let i = 0; i < str.length; i++) {
@@ -29,7 +32,26 @@ function countVowels(str) {
   }
   return count;
 }
-console.log("hello: " + countVowels("hello") + " vowels.");
+console.log("goku: " + countVowels("goku") + " vowels.");
 console.log("world: " + countVowels("world") + " vowels.");
-console.log("javascript: " + countVowels("javascript") + " vowels.");
-*/
+console.log("mission: " + countVowels("mission") + " vowels.");
+
+
+//Excerise 3:
+function sortNumbers(numbers) {
+    return numbers.sort((a, b) => a - b);
+  }
+
+  console.log("Original Array: [9, 4, 6, 2] output Sorted Array: ", sortNumbers([9, 4, 6, 2]));
+  console.log("Original Array: [1, 5, 3, 7, 2] output Sorted Array: ", sortNumbers([1, 5, 3, 7, 2]));
+  console.log("Original Array: [8, 2, 1, 9, 5] output Sorted Array: ", sortNumbers([8, 2, 1, 9, 5]));
+
+
+//Excerise 4:
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+  }
+
+  console.log("Celsius: 30 = Fahrenheit: " + celsiusToFahrenheit(30).toFixed(1));
+  console.log("Celsius: 24 = Fahrenheit: " + celsiusToFahrenheit(24).toFixed(1));
+  console.log("Celsius: 18 = Fahrenheit: " + celsiusToFahrenheit(18).toFixed(1));
